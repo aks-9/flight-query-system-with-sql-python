486824dea588d366f61d7586ef95ed0b08d408eb
ashish <ashishsinghgermany@gmail.com>
2026-02-09 00:24:15 +0100

    first commit: get flight by ID done

---
+++ 1. Python Data Layer.py
@@ -0,0 +1,8 @@
+#Day 1 - Python Data Layer
+#create a Python program that can query the database using code.
+#
+# Note: A flight is considered delayed if it is delayed by 20 minutes or more.
+# Note: An empty value should not be treated as a delay.
+
+#Create a Data Access Layer (DAL)
+#Your goal is to build a Data Access Layer — a set of functions that interact with the database and return query results.
